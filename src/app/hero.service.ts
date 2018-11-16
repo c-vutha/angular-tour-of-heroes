584b6193030a2e9f9cb9c05448a1770f13fa8dfb
Vutha <chhuonvutha46@gmail.com>
2018-11-16 15:20:31 +0700

    recomiit

---
+++ src/app/hero.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './model/hero';
+import { HEROES } from './model/mock-heroes';
+
+@Injectable( {
+  providedIn: 'root'
+} )
+export class HeroService {
+
+  constructor () { }
+}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
-import { Hero } from './../entity/hero';
+import { Hero } from './../model/hero';
 import { Component, OnInit } from '@angular/core';
-import { HEROES } from './../entity/mock-heroes';
+import { HEROES } from './../model/mock-heroes';
 
 @Component({
   selector: 'app-heroes',
